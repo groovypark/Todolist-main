fdcccc756ca4a6b0d747107fe5a462697fe12f04
groovypark <soobin950@gmail.com>
2017-11-17 01:36:26 +0900

    Fix eslint error

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,98 +1,99 @@
 <template>
-    <div class = "main">
-        <div class = "calendar">
-        <select v-model="selected">
-          <option v-for="option in options" v-bind:value="option.text">
-            <h2>{{ option.text }}</h2>
+  <div class = "main">
+    <div class = "calendar">
+      <select v-model="selected">
+        <option v-for="option in options" v-bind:value="option.text">
+          <h2>{{ option.text }}</h2>
           </option>
-        </select>
-          <ul>
-            <li><button><h2>{{ date }}</h2></button></li>
-          </ul>
-        </div>
-        <div class = "todo" v-for="todo in todos">
-            <ul>
-              <li><radial-progress-bar :diameter="70"
-                        :completed-steps="completedSteps"
-                        :total-steps="totalSteps"
-                        >
-                <stop offset="30%" :stop-color="startColor"/>
-                <stop offset="100%" :stop-color="stopColor"/>
-                {{ completedSteps }}%
-              </radial-progress-bar></li>
-              <li><h3>{{todo.title}}</h3></li>
-            </ul>
-        </div>
-        <img src="../assets/icon/Floating.png">
+      </select>
+      <ul>
+        <li><button><h2>{{ date }}</h2></button></li>
+      </ul>
     </div>
+    <div class = "todo" v-for="todo in todos">
+      <ul>
+        <li>
+          <radial-progress-bar :diameter="70"
+            :completed-steps="completedSteps"
+            :total-steps="totalSteps"
+            >
+            <stop offset="30%" :stop-color="startColor"/>
+            <stop offset="100%" :stop-color="stopColor"/>
+            {{ completedSteps }}%
+          </radial-progress-bar>
+        </li>
+        <li><h3>{{todo.title}}</h3></li>
+      </ul>
+    </div>
+    <img src="../assets/icon/Floating.png">
+  </div>
 </template>
 
 <script>
-    import RadialProgressBar from 'vue-radial-progress/dist/vue-radial-progress.min.js'
+  import RadialProgressBar from 'vue-radial-progress/dist/vue-radial-progress.min.js'
 
-    export default {
-    //   props: {
-    //     startColor: {
-    //       type: String,
-    //       required: false,
-    //       default: '#ef6327'
-    //     },
-    //     stopColor: {
-    //       type: String,
-    //       required: false,
-    //       default: '#ef6327'
-    //     }
-    //   },
-      computed: {
-        date() {
-          var today = new Date();
-          var date = today.getDate();
-          var dates = [date-3, date-2, date-1, date, date+1, date+2, date+3];
-          return dates;
-        }
-        // month() {
-        //   var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        //   var today = new Date();
-        //   return monthNames[today.getMonth()];
-        // }
-      },
-      name: 'Main',
-      data () {
-        return {
-          completedSteps: 50,
-          totalSteps: 100,
-          todos: [
-              { title: 'UI 디자인 완료하기' },
-              { title: 'Mini project' },
-              { title: '세미나 발표 준비하기' }
-          ],
-          selected: 'November',
-          options: [
-            { text: 'January' },
-            { text: 'February' },
-            { text: 'March' },
-            { text: 'April' },
-            { text: 'May' },
-            { text: 'June' },
-            { text: 'July' },
-            { text: 'August' },
-            { text: 'September' },
-            { text: 'October' },
-            { text: 'November' },
-            { text: 'December' },
-          ]
-        }
-      },
-      components: {
-        RadialProgressBar
-      },
-      methods: {
-        getDataFromServer: function() {
-          
-          this.date = this.getDataFromServer(dd);
-        }
+  export default {
+  //   props: {
+  //     startColor: {
+  //       type: String,
+  //       required: false,
+  //       default: '#ef6327'
+  //     },
+  //     stopColor: {
+  //       type: String,
+  //       required: false,
+  //       default: '#ef6327'
+  //     }
+  //   },
+    computed: {
+      date () {
+        var today = new Date()
+        var date = today.getDate()
+        var dates = [ date - 3, date - 2, date - 1, date, date + 1, date + 2, date + 3 ]
+        return dates
       }
+      // month() {
+      //   var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      //   var today = new Date();
+      //   return monthNames[today.getMonth()];
+      // }
+    },
+    name: 'Main',
+    data () {
+      return {
+        completedSteps: 50,
+        totalSteps: 100,
+        todos: [
+          { title: 'UI 디자인 완료하기' },
+          { title: 'Mini project' },
+          { title: '세미나 발표 준비하기' }
+        ],
+        selected: 'November',
+        options: [
+          { text: 'January' },
+          { text: 'February' },
+          { text: 'March' },
+          { text: 'April' },
+          { text: 'May' },
+          { text: 'June' },
+          { text: 'July' },
+          { text: 'August' },
+          { text: 'September' },
+          { text: 'October' },
+          { text: 'November' },
+          { text: 'December' }
+        ]
+      }
+    },
+    components: {
+      RadialProgressBar
     }
+    // methods: {
+    //   getDataFromServer: function() {
+    //     this.date = this.getDataFromServer(dd);
+    //   }
+    // }
+  }
 </script>
 
 <style scoped>
